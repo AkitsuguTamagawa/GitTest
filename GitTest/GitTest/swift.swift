02f728bbda89f4be573e259f31d867f9f519b07c
akitsugu.tamagawa <tamagawa@spacemarket.co.jp>
2015-12-10 23:26:28 +0900

    add swift

---
+++ GitTest/GitTest/swift.swift
@@ -0,0 +1,21 @@
+//
+//  swift.swift
+//  GitTest
+//
+//  Created by akitsugu.tamagawa on 2015/12/10.
+//  Copyright © 2015年 akitsugu.tamagawa. All rights reserved.
+//
+
+import UIKit
+
+class swift: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
