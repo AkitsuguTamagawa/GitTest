4d7819c08732ff366ab7624e29ed9188cb207c69
akitsugu.tamagawa <tamagawa@spacemarket.co.jp>
2015-12-10 23:13:33 +0900

    PR2

--- GitTest/GitTest/ViewController.swift
+++ GitTest/GitTest/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         //testCommit
         //
+        //PR2
     }
 
     override func didReceiveMemoryWarning() {
